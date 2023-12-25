ac10b944e781eaef6bf697a30372e9dab6af1cf4
sabamdarif <mdarif4102004@gmail.com>
2023-12-25 15:16:07 +0530

    Update Installer

--- setup.sh
+++ setup.sh
@@ -3,14 +3,15 @@ R="$(printf '\033[1;31m')"
 G="$(printf '\033[1;32m')"
 Y="$(printf '\033[1;33m')"
 B="$(printf '\033[1;34m')"
-C="$(printf '\033[1;36m')"                                        W="$(printf '\033[1;37m')"
+C="$(printf '\033[1;36m')"                                       
+W="$(printf '\033[1;37m')"
 
 banner() {
 clear
+printf "\033[32m code by @sabamdrif \033[0m\n"
+printf "\033[32m subscribe my YouTube Channel Hello Android \033[0m\n"
 
 }
-
-
 
 check_pack() {
 	banner
