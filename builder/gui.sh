8a3f4828ff4b06229bde881624ea665e6c0e40af
sabamdarif <mdarif4102004@gmail.com>
2024-01-31 09:28:55 +0530

    Fix DE installation issue

--- builder/gui.sh
+++ builder/gui.sh
@@ -8,7 +8,7 @@ C="$(printf '\033[1;36m')"
 
 banner() {
 clear
-printf "\033[32m code by @sabamdrif \033[0m\n"
+printf "\033[32m code by @saba_mdrif \033[0m\n"
 printf "\033[32m subscribe my YouTube Channel Hello Android \033[0m\n"
 
 }
@@ -26,94 +26,26 @@ add_user() {
     apt install sudo -y
     echo "$user ALL=(ALL:ALL) ALL" >> /etc/sudoers
     cat <<EOF > "/data/data/com.termux/files/usr/bin/debian"
-if [ "\$1" == "-r" ]; then
+if [ "\$1" = "-r" ]; then
     proot-distro login debian
 else
     proot-distro login --user "$user" debian
-fi 
+fi
 EOF
+    #chmod +x /data/data/com.termux/files/usr/bin/debian
     clear
 
 }
 
-fix_broken() {
-    banner
-    echo -e "${Y}Checking error and fix it..."${W}
-    sudo dpkg --configure -a
-     
-    sudo apt-get install --fix-broken keyboard-configuration -y
-}
-
-package() {
-  banner
-    echo -e "${R} [${W}-${R}]${C} Checking required packages..."${W}
-    apt update -y
-    apt install sudo -y
-    apt --fix-broken install udisks2 -y
-    rm /var/lib/dpkg/info/udisks2.postinst
-    echo "" > /var/lib/dpkg/info/udisks2.postinst
-    sudo dpkg --configure -a
-    sudo apt-mark hold udisks2
-    sudo apt-mark unhold gvfs-daemons
-    sudo dpkg --configure -a
-    packs=(sudo wget curl nano git qterminal mousepad librsvg2-common inetutils-tools dialog tightvncserver tigervnc-standalone-server tigervnc-tools dbus-x1)
-    sudo dpkg --configure -a
-    for pack_name in "${packs[@]}"; do
-        type -p "$pack_name" &>/dev/null || {
-            echo -e "\n${R} [${W}-${R}]${G} Installing package : ${Y}$pack_name${C}"${W}
-            sudo apt-get install "$pack_name" -y --no-install-recommends
-        }
-    done
-    fix_broken
-}
-
-firefox_install() {
-		clear
-		banner
-		sleep 1
-		echo "${Y}Checking if  Firefox browser installed already.."${W}
-		echo
-		echo
-		if [[ $(command -v firefox) ]]; then
-			echo "${C}Firefox is already installed.."${W}
-			sleep .5
-			clear
-		else
-			clear
-			sleep 1
-			echo "${G}Firefox not found.Installing now.."${W}
-			echo
-			echo
-			sudo apt update;sudo apt install firefox-esr -y 
-		fi
-}
-	
-vlc_installer() {
-	clear
-	banner
-  read -p "${G}Do you to install VLC (y/n) "${w} answer
-if [ "$answer" == "y" ]; then
-	echo "${Y}Checking if vlc is available or not"${W}
-	if [[ $(command -v vlc) ]]; then
-		echo
-		echo "${G}vlc is already Installed"${W}
-		sleep 1
-	else
-		echo "${G}vlc  is not installed. Installing vlc.."${W}
-		echo
-		sleep 1
-		sudo apt update && sudo apt install vlc -y
-	fi
-else
-    echo "${C}Canceling...."${W}
-    sleep 1.2
-fi
-}
-
-select_desktop_type() {
-	clear
+update_sys() {
+  echo "${G}Updating System..."${W}
   termux-wake-lock
-	banner
+  sleep 3
+  apt-get update
+}
+
+ask() {
+  banner
 	echo
 	echo -e "${R} [${W}-${R}]${C} Select Desktop Type"${W}
 	echo
@@ -125,29 +57,133 @@ select_desktop_type() {
 	echo
 	echo "${C}4. KDE"${W}
 	echo
-	echo "${C}5. GNOME (Beta)"${W}
+	echo "${C}5. GNOME"${W}
 	echo
-	read -p "${Y}Select option(default 1): "${W} select_method
+	read -p "${Y}Select option(default 1): "${W} select_desktop
 	echo
-	sleep 1.5
-	if [[ $select_method == "1" ]]; then
+  sleep 0.5
+  banner
+  read -p "${G}Do you to install VLC (y/n) "${w} ask_vlc
+  sleep 0.5
+  echo -e "${R} [${W}-${R}]${C} Select Browser"${W}
+	echo
+	echo "${C}1. Firefox (recommended)"${W}
+	echo
+	echo "${C}2. Chromium"${W}
+	echo
+	read -p "${Y}Select option(default 1): "${W} ask_browser
+}
+
+install_desktop_type() {
+	banner
+	if [[ $select_desktop == "1" ]]; then
 		xfce_mode
-	fi
-	if [[ $select_method == "2" ]]; then
+	elif [[ $select_desktop == "2" ]]; then
 		lxde_mode
-	fi
-	if [[ $select_method == "3" ]]; then
+	elif [[ $select_desktop == "3" ]]; then
 		lxqt_mode
-	fi
-	if [[ $select_method == "4" ]]; then
+	elif [[ $select_desktop == "4" ]]; then
 		kde_mode
-	fi
-	if [[ $select_method == "5" ]]; then
+	elif [[ $select_desktop == "5" ]]; then
 		gnome_mode
-	fi
-	if [[ $select_method == "" ]]; then
+	elif [[ $select_desktop == "" ]]; then
 		xfce_mode
 	fi
+}
+
+fix_broken() {
+    banner
+    echo -e "${Y}Checking error and fix it..."${W}
+     dpkg --configure -a
+     apt-get install --fix-broken -y
+}
+
+package() {
+  banner
+    echo -e "${R} [${W}-${R}]${C} Checking required packages..."${W}
+    apt update -y
+    apt --fix-broken install udisks2 -y
+    rm /var/lib/dpkg/info/udisks2.postinst
+    echo "" > /var/lib/dpkg/info/udisks2.postinst
+     dpkg --configure -a
+     apt-mark hold udisks2
+     apt-mark unhold gvfs-daemons
+     dpkg --configure -a
+    packs=(sudo wget curl nano git mousepad librsvg2-common inetutils-tools dialog tightvncserver tigervnc-standalone-server tigervnc-tools dbus-x11 )
+     dpkg --configure -a
+    for packs_name in "${packs[@]}"; do
+        type -p "$packs_name" &>/dev/null || {
+            echo -e "\n${R} [${W}-${R}]${G} Installing package : ${Y}$packs_name${C}"${W}
+             apt-get install "$packs_name" -y --no-install-recommends
+        }
+    done
+    fix_broken
+}
+
+firefox_installer() {
+    echo "${Y}Checking if  Firefox browser installed already.."${W}
+		echo
+		echo
+		if [[ $(command -v firefox) ]]; then
+			echo "${C}Firefox is already installed.."${W}
+			sleep 0.5
+			clear
+		else
+			clear
+			echo "${G}Firefox not found.Installing now.."${W}
+			echo
+			echo
+       apt install firefox-esr -y 
+		fi
+}
+
+chromium_installer() {
+  echo "${Y}Checking if  Chromium browser installed already.."${W}
+		echo
+		echo
+		if [[ $(command -v chromium) ]]; then
+			echo "${C}Chromium is already installed.."${W}
+			sleep 0.5
+			clear
+		else
+			clear
+			echo "${G}Chromium not found.Installing now.."${W}
+			echo
+			echo
+       apt install chromium -y 
+		fi
+}
+
+browser_installer() {
+		banner
+    if [[$ask_browser == "1"]]; then
+    firefox_installer
+    elif [[$ask_browser == "2"]]; then 
+    chromium_installer
+    elif [[$ask_browser == ""]]; then
+    firefox_installer
+    fi
+    
+}
+	
+vlc_installer() {
+	banner
+if [ "$ask_vlc" == "y" ]; then
+	echo "${Y}Checking if vlc is available or not"${W}
+	if [[ $(command -v vlc) ]]; then
+		echo
+		echo "${G}vlc is already Installed"${W}
+		sleep 1
+	else
+		echo "${G}vlc  is not installed. Installing vlc.."${W}
+		echo
+		sleep 1
+    apt install vlc -y
+	fi
+else
+    echo "${C}Canceling...."${W}
+    sleep 1.2
+fi
 }
 
 vncstop() {
@@ -173,8 +209,7 @@ xfce_mode() {
   package
   banner
 	echo -e "${R} [${W}-${R}]${C} Installing XFCE DESKTOP"${W}
-	apt update
-       sudo apt install xfce4* -y
+        apt install xfce4* -y
        banner
        echo -e "${R} [${W}-${R}]${C} Setting up VNC Server..."${W}
   if [[ ! -d "$HOME/.vnc" ]]; then
@@ -194,18 +229,40 @@ xfce_mode() {
 }
 
 gnome_mode() {
-  add_user
 	banner
-	echo -e "${R} [${W}-${R}]${C} Installing GNOME DESKTOP"${W}
-	apt update
-	apt install gnome-shell gnome-terminal gnome-tweaks -y
+  echo "${G}Select Gnome Desktop Type..."${W}
+	echo
+	echo "${Y}1. Core (~2Gb | Recomended)"${W}
+	echo
+	echo "${Y}2. Full (~3.5GB of space)"${W}
+	echo
+	read -p "${Y}select an option (Default 1): "${W} answer_gnome_desktop
+	echo
+  if [[ ${answer_gnome_desktop} == "1" ]]; then
+  banner
+        echo "${G}Installing Gnome Core..."${W}
+        echo
+        apt install gnome-shell gnome-terminal -y
+    elif [[ ${answer_gnome_desktop} == "2" ]]; then
+    banner
+        echo "${G}Installing Gnome Full..."${W}
+        echo
+        apt-get install gnome -y
+    elif [[ ${answer_gnome_desktop} == "" ]]; then
+    banner
+        echo "${G}Installing Gnome Core..."${W}
+        echo
+        apt install gnome-shell gnome-terminal -y
+    fi
 	dpkg --configure -a
 	apt --fix-broken install -y
-	packs=(wget curl nautilus nano gedit tigervnc-standalone-server tigervnc-tools dbus-x11 )
-	for pack in "${packs[@]}"; do
-        type -p "$pack" &>/dev/null || {
-            echo -e "\n${R} [${W}-${R}]${G} Installing package : ${Y}$pack${C}"${W}
-            sudo apt-get install "$pack" -y --no-install-recommends
+  banner
+  echo "${Y}Installing Required Packages"${W}
+	packs=(wget curl nautilus nano gedit gnome-software gnome-tweaks gnome-shell-extension-manager tigervnc-standalone-server tigervnc-tools dbus-x11 )
+	for packs_name in "${packs[@]}"; do
+        type -p "$packs_name" &>/dev/null || {
+            echo -e "\n${R} [${W}-${R}]${G} Installing package : ${Y}$packs_name${C}"${W}
+             apt-get install "$packs_name" -y --no-install-recommends
         }
     done
     echo -e "${R} [${W}-${R}]${C} Setting up VNC Server..."${W}
@@ -218,13 +275,12 @@ fi
 cat <<EOF > "$HOME/.vnc/xstartup"
 export XDG_CURRENT_DESKTOP="GNOME"
 service dbus start
-echo "gnome-shell --x11    
+gnome-shell --x11    
 EOF
 chmod +x "$HOME/.vnc/xstartup"
-
-mkdir -p "/home/$user/.vnc"
-cp -r "$HOME/.vnc/xstartup" "/home/$user/.vnc/xstartup"
-chmod +x "/home/$user/.vnc/xstartup"
+# mkdir -p "/home/$user/.vnc"
+# cp -r "$HOME/.vnc/xstartup" "/home/$user/.vnc/xstartup"
+# chmod +x "/home/$user/.vnc/xstartup"
    if [[ -e "/bin/vncstart" ]]; then
         rm -rf /bin/vncstart
     fi
@@ -232,6 +288,7 @@ chmod +x "/home/$user/.vnc/xstartup"
   echo "vncserver -geometry 2580x1080 " >>/bin/vncstart
     chmod +x /bin/vncstart
   vncstop
+  #echo "export DISPLAY=":1"" >> /etc/profile
     echo "export PULSE_SERVER=127.0.0.1" >> /etc/profile
     source /etc/profile
  echo -e "${R} [${W}-${R}]${C} Fix Vnc Login Issue.."${W}
@@ -245,13 +302,12 @@ lxde_mode() {
   package
   banner
 	echo -e "${R} [${W}-${R}]${C} Installing LXDE DESKTOP"${W}
-	apt update
-	sudo apt install lxde lxterminal debia-themes -y
+	 apt install lxde lxterminal -y
 	apt-get install udisks2 -y
 	echo " " > /var/lib/dpkg/info/udisks2.postinst
 	apt-mark hold udisks2
-	apt-get install sudo tzdata -y
-	apt-get install lxde lxterminal debia-themes -y
+	apt-get install  tzdata -y
+	apt-get install lxde lxterminal -y
 	mv /usr/bin/lxpolkit /usr/bin/lxpolkit.bak
 	apt-get --fix-broken install -y
 	apt-get clean
@@ -278,12 +334,11 @@ lxqt_mode(){
   package
   banner
 	echo -e "${R} [${W}-${R}]${C} Installing LXQT DESKTOP"${W}
-	apt-get update
 	apt-get install udisks2 -y
 	echo " " > /var/lib/dpkg/info/udisks2.postinst
 	apt-mark hold udisks2
-	apt-get install sudo tzdata -y
-	apt-get install lxqt qterminal debia-themes -y
+	apt-get install  tzdata -y
+	apt-get install lxqt qterminal -y
 	apt-get install tigervnc-standalone-server dbus-x11 -y
 	apt-get --fix-broken install -y
 	apt-get clean
@@ -310,11 +365,10 @@ kde_mode() {
   package
   banner
 	echo -e "${R} [${W}-${R}]${C} Installing KDE DESKTOP"${W}
-	apt update 
 	apt-get install udisks2 -y
 	echo " " > /var/lib/dpkg/info/udisks2.postinst
 	apt-mark hold udisks2
-	apt-get install sudo tzdata -y
+	apt-get install tzdata -y
 	apt-get install kde-plasma-desktop konsole -y
 	apt-get install tigervnc-standalone-server dbus-x11 -y
 	apt-get --fix-broken install -y
@@ -340,42 +394,150 @@ kde_mode() {
 note() {
 banner
     echo -e " ${G} Successfully Installed !"${W}
+    termux-wake-unlock
     sleep 1
+    if [[ $select_desktop == "5" ]]; then
     echo
-    echo -e " ${G}Type ${C}debian${G} to login as normal user"${W}
+    echo -e " ${C}Type ${G}debian${C} to login into debian cli (as root user)"${W}
     echo
-    echo -e " ${G}Type ${C}debian -r${G} to login as root user"${W}
+    echo -e " ${C}You cannot add any user in GNOME Desktop"${W}
     echo
-    echo -e " ${G}Type ${C}vncstart${G} to run Vncserver."${W}
+    echo -e " ${C}You need to use as root user"${W}
+    else
     echo
-    echo -e " ${G}Type ${C}vncstop${G} to stop Vncserver."${W}
+    echo -e " ${C}Type ${G}debian${C} to login as normal user"${W}
     echo
-    echo -e " ${C}Install VNC VIEWER OR Nethunter Kex on your Device."${W}
+    echo -e " ${C}Type ${G}debian -r${C} to login as root user"${W}
     echo
-    echo -e " ${C}Open VNC VIEWER & Click on + Button."${W}
+    fi
+    echo
+    echo -e " ${C}Type ${G}vncstart${C} to run Vncserver."${W}
+    echo
+    echo -e " ${C}Type ${G}vncstop${C} to stop Vncserver."${W}
+    echo
+    echo -e " ${C}Open VNC VIEWER or Nethunter Kex & Click on + Button."${W}
     echo
     echo -e " ${C}Enter the Address localhost:1 & Name anything you like."${W}
     echo
-    echo -e " ${C}Set the Picture Quality to High for better Quality."${W}
-    echo 
     echo -e " ${C}Click on Connect & Input the Password."${W}
     echo 
-    echo -e " ${C}If you install the GNOME DESKKTOP use UltraVnc mode in Nethunter Kex"${W}
+    echo -e " ${C}If you install the GNOME DESKKTOP you may need to use UltraVnc mode in Nethunter Kex."${W}
     echo
-    echo -e " ${C}Enjoy :D"${W}
+    echo -e " ${C}Enjoy"${W}
     echo
     echo
-termux-wake-unlock
+
 }
 
 add_sound() {
-	echo "$(echo "bash ~/.sound" | cat - /data/data/com.termux/files/usr/bin/debia)" > /data/data/com.termux/files/usr/bin/debia
+	echo "$(echo "bash ~/.sound" | cat - /data/data/com.termux/files/usr/bin/debian)" > /data/data/com.termux/files/usr/bin/debian
+}
+
+checkup_xfce() {
+  if [[ $select_desktop == "1" ]]; then
+    echo "${Y}Do you want to set up the application menu?"${W}
+    customize
+  else
+    echo "Canceling..."
+    sleep 0.5
+  fi
+}
+
+
+customize() {
+	if [[ $(command -v plank) ]]; then
+	echo "${G}Plank is already installed .."${W}
+        sleep .5 
+        clear
+   else
+	   clear
+	   sleep 1 
+	   echo "${G}Plank not found.Installing now.."${W}
+	   echo 
+	    apt install plank -y
+	fi
+mkdir /home/${user}/.config/autostart/
+        cat <<EOF > "/home/${user}/.config/autostart/plank.desktop"
+[Desktop Entry]
+Type=Application
+Name=Plank
+Exec=plank
+EOF
+chmod +x /home/${user}/.config/autostart/plank.desktop
+ echo "${G}Create Your VNC Password"${W}
+    vncstart
+    sleep 60
+    check-up
+    vncstop
+  gsettings set net.launchpad.plank.dock.settings:/net/launchpad/plank/docks/dock1/ theme 'Gtk+'
+	gsettings set net.launchpad.plank.dock.settings:/net/launchpad/plank/docks/dock1/ zoom-enabled true
+  banner
+	sudo apt install rofi -y
+	sudo apt install wget -y
+	mkdir -p ~/.config/rofi
+        rofi -dump-config > ~/.config/rofi/config.rasi
+	cd ~
+	wget https://raw.githubusercontent.com/sabamdarif/modded-debian/main/images/application-menu.png
+	wget https://raw.githubusercontent.com/sabamdarif/modded-debian/main/builder/style.rasi
+	mv style.rasi application-menu.png ~/.config/rofi
+cat <<EOF > "/home/${user}/.config/rofi/launcher.sh"
+#!/usr/bin/env bash
+
+	## Run
+	rofi \
+    -show drun \
+    -theme /home/${user}/.config/rofi/style.rasi
+EOF
+cat <<EOF > "appsearch.desktop"
+[Desktop Entry]
+Name=Application Search
+Exec=bash /home/${user}/.config/rofi/launcher.sh
+Icon=/home/${user}/.config/rofi/application-menu.png
+Type=Application
+Terminal=false
+StartupNotify=false
+EOF
+mkdir ~/.local/share/applications
+if [[ ! -d "~/.local/share/applications" ]]; then
+    mkdir ~/.local/share/applications
+fi
+mv appsearch.desktop ~/.local/share/applications/
+chmod +x ~/.local/share/applications/appsearch.desktop
+config_folder="/home/${user}/.config"
+plank_folder="${config_folder}/plank"
+dock_folder="${plank_folder}/dock1"
+launchers_folder="${dock_folder}/launchers"
+if [ ! -d "${config_folder}" ]; then
+    mkdir -p "${config_folder}"
+fi
+
+if [ ! -d "${plank_folder}" ]; then
+    mkdir -p "${plank_folder}"
+fi
+
+if [ ! -d "${dock_folder}" ]; then
+    mkdir -p "${dock_folder}"
+fi
+
+if [ ! -d "${launchers_folder}" ]; then
+    mkdir -p "${launchers_folder}"
+fi
+cd ~
+cat <<EOF > appsearch.dockitem
+[PlankDockItemPreferences]
+Launcher=file:///home/${user}/.local/share/applications/appsearch.desktop
+EOF
+mv appsearch.dockitem ~/.config/plank/dock1/launchers
+clear
+killall plank
 
 }
 
-#full_update
-select_desktop_type
-firefox_install
+ask
+update_sys
+install_desktop_type
+browser_installer
 vlc_installer
 add_sound
+checkup_xfce
 note
